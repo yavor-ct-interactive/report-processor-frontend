4632822ae8f895a0c4da4b18b0dcf73f4f8b55a4
Yavor Mihaylov <yavor.mihaylov@ct-interactive.com>
2025-09-03 14:33:32 +0300

    added a theme component

---
+++ src/themes/MainTheme.tsx
@@ -0,0 +1,20 @@
+import { createTheme } from "flowbite-react";
+
+export const CustomModalTheme = createTheme({
+        modal: {
+          root: {
+            base: "fixed inset-x-0 top-0 z-50 h-screen overflow-y-auto overflow-x-hidden md:inset-0 md:h-full",
+            
+          },
+          header: {
+            base: "flex items-start justify-between rounded-t border-b p-5 dark:border-gray-600 text-center",
+            title: "text-blue-600 text-xl font-medium text-gray-900 dark:text-white w-full text-center ",
+            close:{
+              base: "bg-red-200 p-0",
+              icon: "w-3 h-3",
+            }
+          },
+          
+        },
+      });
+
