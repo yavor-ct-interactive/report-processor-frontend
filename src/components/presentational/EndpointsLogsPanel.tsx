33fa0442f749450a86337339a1b811833aace1f1
Yavor Mihaylov <yavor.mihaylov@ct-interactive.com>
2025-11-27 15:14:04 +0200

    updated EnpointsLogs Table

--- src/components/presentational/EndpointsLogsPanel.tsx
+++ src/components/presentational/EndpointsLogsPanel.tsx
@@ -7,7 +7,6 @@ import { ImSpinner9 } from "react-icons/im";
 import { QueryClient, QueryClientProvider, useQuery, useQueryClient, keepPreviousData } from "@tanstack/react-query";
 
 import { IndeterminateCheckbox } from "../panels/functional/IndeterminateCheckbox.tsx"
-import { EditableTable} from "../tables/editableTable.tsx";
 import { RowSelection, createColumnHelper } from "@tanstack/react-table";
 import { DateTimePicker, RPDatePicker } from './widgets/RPDatePicker.tsx';
 import {RPSelect} from './SelectElement.tsx';
@@ -15,6 +14,7 @@ import dayjs from "dayjs"; // For date formatting
 import { PromosForm } from '../Forms/PromosForm.tsx';
 import { NewPromosForm } from '../Forms/NewPromoForm.tsx';
 import { AbstractTable } from '../tables/abstractTable.tsx';
+import { EndpointLogsTable } from '../tables/EndpointLogsTable.tsx';
 
 const data_url = '/backend/sysinfo/get-cmd-exec-stats'
 
@@ -28,6 +28,8 @@ export type EndpointLogsCols = {
     id: number;
     command: string;
     unique_identifier: string | null; 
+    sender_ip: string;
+    uploaded_filename: string;
     ended_at: Date;
     started_at: Date, 
     result: string,
@@ -74,16 +76,24 @@ export const EndpointsLogsPanel:FC<PropsWithChildren<{}>> = ({children}) => {
     });
 
     const columns = [
-        columnHelper.accessor("id", {
-          header: "ID",
+        columnHelper.accessor("command", {
+          header: "Endpoint",
+          aggregationFn: "count",
+          cell: (info) => info.getValue(),
+          size: 100,
+          minSize: 100, 
+          maxSize: 100
+        }),
+        columnHelper.accessor("sender_ip", {
+          header: "Sender IP",
           aggregationFn: "count",
           cell: (info) => info.getValue(),
           size: 50,
-          minSize: 30, 
-          maxSize: 75
+          minSize: 50, 
+          maxSize: 50
         }),
-        columnHelper.accessor("command", {
-          header: "endpoint",
+        columnHelper.accessor("uploaded_filename", {
+          header: "Filename",
           aggregationFn: "count",
           cell: (info) => info.getValue(),
           size: 100,
@@ -93,15 +103,23 @@ export const EndpointsLogsPanel:FC<PropsWithChildren<{}>> = ({children}) => {
         columnHelper.accessor("unique_identifier", {
           header: "Unique identifier",
           aggregationFn: "count",
-          cell: (info) => info.getValue(),
+          cell: (info) => (
+             <div className="flex justify-center items-center w-full">
+                {info.getValue()}
+             </div>
+          ),
           size: 50,
-          minSize: 70, 
-          maxSize: 75
+          minSize: 50, 
+          maxSize: 50
         }),
         columnHelper.accessor("status_code", {
           header: "Status Code",
           //render the Genres component here:
-          cell: (info) => info.getValue() ,
+          cell: (info) => (
+             <div className="flex justify-center items-center w-full">
+                {info.getValue()}
+             </div>
+          ),
           size: 30, 
           minSize: 30, 
           maxSize:30
@@ -109,40 +127,42 @@ export const EndpointsLogsPanel:FC<PropsWithChildren<{}>> = ({children}) => {
         columnHelper.accessor("error_message", {
           header: "Error Message",
           //use our convertTsoHoursAndMinutes function to render the runtime of the show
-          cell: (info) => info.getValue()
-
+          cell: (info) => info.getValue(),
+          size: 200, 
+          minSize: 200, 
+          maxSize: 200
         }),
         columnHelper.accessor("result", {
           header: "Result",
           cell: (info) => info.getValue(),
-          size:70,
-          minSize:70,
-          maxSize:100
+          size:50,
+          minSize:50,
+          maxSize:50
         }), 
         columnHelper.accessor("started_at", {
           header: "Started At",
           cell: (info) => info.getValue(),
-          size: 100,
-          minSize:100,
-          maxSize: 100
+          size: 50,
+          minSize:50,
+          maxSize: 50
         }),   
         columnHelper.accessor("ended_at", {
           header: "Ended At",
           cell: (info) => info.getValue(),
-          size: 100,
-          minSize: 100,
-          maxSize: 100,
+          size: 50,
+          minSize: 50,
+          maxSize: 50,
         }),                    
       ]
     
     return (
       <div>
-        <div className="w-full flex flex-col items-stretch space-y-3 justify-between  overflow-x-scroll dark:border-gray-700">
+        <div className="w-full flex flex-col items-stretch space-y-3 justify-between  overflow-x-hidden dark:border-gray-700">
             {inProgress ? <ImSpinner9 className="loading-icon" /> : ""} 
             <div>
             {promos.isError ? <div>An error occured</div> : ''}
             {promos.isSuccess ? 
-              <AbstractTable                            
+              <EndpointLogsTable                            
                   data={promos?.data}
                   columns={columns}
                   pageIndex={pageIndex}
---
+++ src/components/tables/EndpointLogsTable.tsx
@@ -0,0 +1,322 @@
+import React, { useState, useEffect } from 'react';
+import type { ColumnFiltersState, SortingState, ColumnResizeMode, ColumnResizeDirection, GroupColumnDef} from "@tanstack/react-table"
+import { flexRender, useReactTable } from '@tanstack/react-table';
+import type{ Column } from '@tanstack/react-table';
+import { Modal, Button, Label, TextInput, Select, ModalHeader, ModalBody, ModalFooter, ThemeProvider } from "flowbite-react";
+import type { FilterFn } from '@tanstack/react-table';
+import { CustomButton, FilterButton } from '../presentational/Common/Buttons';
+import { ImSpinner9 } from 'react-icons/im';
+import { CustomModalTheme } from '../../themes/MainTheme';
+import {ColumnFilter, advancedFilter} from './abstractTable'
+/*New code for table filters */
+/*End of new codee for table filters */
+import {
+  getCoreRowModel,
+  getSortedRowModel,
+  getPaginationRowModel,
+  getFilteredRowModel,
+  getGroupedRowModel,
+  getExpandedRowModel,
+} from '@tanstack/react-table';
+import exportToCsv  from "tanstack-table-export-to-csv";
+
+import type {GameplayCols} from '../presentational/Gameplays'
+
+type TableProps<TData> = {
+    data: TData[];
+    columns: GroupColumnDef<TData>[];
+    pageIndex: number;
+    pageSize: number;
+    setPageIndex: React.Dispatch<React.SetStateAction<number>>;
+    setPageSize: React.Dispatch<React.SetStateAction<number>>;
+    nextPage: () => void,
+    prevPage: () => void,
+    totalCount: number,
+};
+/*export function Searchbar({
+    value: initialValue,
+    onChange,
+    ...props
+  }: {
+    value: string | number;
+    onChange: (value: string | number) => void;
+  } & Omit<React.InputHTMLAttributes<HTMLInputElement>, "onChange">) {
+    const [value, setValue] = useState(initialValue);
+    useEffect(() => {
+      setValue(initialValue);
+    }, [initialValue]);
+    //if the entered value changes, run the onChange handler once again.
+    useEffect(() => {
+      onChange(value);
+    }, [value]);
+    //render the basic searchbar:
+    return (
+      <input className="w-full"
+        {...props}
+        value={value}
+        onChange={(e) => setValue(e.target.value)}
+      />
+    );
+  }
+  
+function Filter({ column }: { column: Column<GameplayCols, unknown> }) {
+    const columnFilterValue = column.getFilterValue();
+
+    return (
+      <Searchbar
+        onChange={(value) => {
+          column.setFilterValue(value);
+        }}
+        placeholder={`Search...`}
+        type="text"
+        value={(columnFilterValue ?? "") as string}
+      />
+    );
+  }
+*/
+interface FilterProps {
+  column: Column<GameplayCols, unknown>;
+}
+
+const Filter: React.FC<FilterProps> = ({ column }) => {
+  const columnFilterValue = column.getFilterValue();
+  return (   
+    <div className="w-full flex justify-center">
+      <TextInput className="w-full box-border"
+        type="text"
+        value={(columnFilterValue ?? '') as string}
+        onChange={(e) => column.setFilterValue(e.target.value)}
+        placeholder={`Search...`}
+      />
+    </div>
+  );
+};  
+/* New code added here */
+interface FilterModalProps {
+  columnName: string;
+  columnHeader: string;
+  show: boolean;
+  onClose: () => void;
+  onApply: (filter: { operator: string; value1: string; value2?: string }) => void;
+}
+export const FilterModal: React.FC<FilterModalProps> = ({
+  columnName,
+  columnHeader,
+  show,
+  onClose,
+  onApply,
+}) => {
+  const [operator, setOperator] = useState(">");
+  const [value1, setValue1] = useState("");
+  const [value2, setValue2] = useState("");
+
+  const handleApply = () => {
+    onApply({ operator, value1, value2 });
+    onClose();
+  };
+  const handleClear = () => {
+    onApply(undefined)
+    onClose()
+  }
+  const filter_name = columnName.replaceAll('_',' ')
+  return (
+    <ThemeProvider theme={CustomModalTheme}>
+    <Modal show={show} onClose={onClose} popup size="md">
+      <ModalHeader>
+        Filter by  <span className="font-semibold"> {columnHeader}</span>
+      </ModalHeader>
+      <ModalBody>
+        <div className="space-y-4">
+          <div>
+            <Label htmlFor="operator">Operator</Label>
+            <Select id="operator" value={operator} onChange={(e) => setOperator(e.target.value)}>
+              <option value=">">Greater than</option>
+              <option value="<">Less than </option>
+              <option value="=">Equal to</option>
+              <option value="between">Between</option>
+            </Select>
+          </div>
+
+          <div>
+            <Label htmlFor="value1">Value</Label>
+            <TextInput
+              id="value1"
+              type="text"
+              value={value1}
+              onChange={(e) => setValue1(e.target.value)}
+              placeholder="Enter value..."
+            />
+          </div>
+
+          {operator === "between" && (
+            <div>
+              <Label htmlFor="value2">And</Label>
+              <TextInput
+                id="value2"
+                type="text"
+                value={value2}
+                onChange={(e) => setValue2(e.target.value)}
+                placeholder="Enter second value..."
+              />
+            </div>
+          )}
+        </div>
+      </ModalBody>
+      <ModalFooter>
+        <CustomButton onClick={handleApply}>
+          Apply
+        </CustomButton>
+        <CustomButton onClick={() => handleClear()}>
+          Clear
+        </CustomButton>
+        <CustomButton onClick={onClose}>
+          Cancel
+        </CustomButton>
+      </ModalFooter>
+    </Modal>
+    </ThemeProvider>
+  );
+};
+
+
+/*End of new code */
+export function EndpointLogsTable<TData>({ columns, data}: TableProps<TData>) {
+  const [sorting, setSorting] = useState<SortingState>([]);
+  const [pagination, setPagination] = useState({
+    pageIndex: 0,
+    pageSize: 100,
+  });
+  const [columnFilters, setColumnFilters] = useState<ColumnFiltersState>([]);
+
+  const table = useReactTable({
+    data: data?.items ?? [],
+    columns,
+    filterFns: {
+      advanced: advancedFilter,
+    },
+    enableColumnResizing: true,
+    debugTable: true,
+    debugHeaders: true,
+    debugColumns: true,
+    state: {
+      sorting,
+      pagination,
+      columnFilters,
+    },
+    columnResizeMode: 'onChange',
+    // table.getFilteredRowModel().rows.length} / {data?.items?.length ?? 0
+    onSortingChange: setSorting,
+    onPaginationChange: setPagination,
+    onColumnFiltersChange: setColumnFilters,
+    getCoreRowModel: getCoreRowModel(),
+    getSortedRowModel: getSortedRowModel(),
+    getFilteredRowModel: getFilteredRowModel(),
+    getPaginationRowModel: getPaginationRowModel(),
+    // Include grouping/expanded features if needed
+    getGroupedRowModel: getGroupedRowModel(),
+    getExpandedRowModel: getExpandedRowModel(),
+  });
+  const string_filterable_columns = ['gameplay_id', 'operator', 'operator_endpoint', 'operator_player_id', 'currency', 'game', 'game_start_time'];
+  const number_filterable_columns = ['rgs_total_bet', 'game_denomination', 'win_transaction_amount', 'jp']
+
+  const handleExportToCsv = (): void => {
+    const headers = table
+      .getHeaderGroups()
+      .map((x) => x.headers)
+      .flat();
+
+    const rows = table.getFilteredRowModel().rows;
+
+    exportToCsv(`gameplays_data_${Date.now()}`, headers, rows);
+  };
+return (
+    <div className="relative overflow-y-auto max-h-[75vh] min-h-[75vh] border w-full border-gray-700 rounded-lg">
+      <div className='flex flex-row items-center justify-center gap-4 p-4'>
+          <div>
+            Filtered records: {table.getFilteredRowModel().rows.length}
+          </div>
+          <CustomButton onClick={handleExportToCsv}>Export to csv</CustomButton>
+
+      </div>
+      <table className=" text-sm text-left text-gray-500  dark:text-gray-200 table-fixed mx-auto w-full" >
+        <thead className="sticky top-0 text-m text-gray-700 bg-neutral-100 h-8 bg-gray-50 dark:bg-teal-900 dark:text-gray-400 z-10">
+          {table.getHeaderGroups().map((hg) => (
+            <tr key={hg.id} >
+              {hg.headers.map((header) => (
+                <th key={header.id} colSpan={header.colSpan} style={{ width: header.getSize() }} 
+                 className="overflow-hidden text-ellipsis whitespace-wrap p-2" >                  
+                  {header.column.getCanFilter() &&
+                      string_filterable_columns.includes(header.id as string) && (
+                        <div>
+                          <Filter column={header.column} />
+                        </div>
+                  )}
+                  {header.column.getCanFilter() &&
+                    number_filterable_columns.includes(header.id as string) && (
+                      <div  >
+                        <ColumnFilter column={header.column} />
+                      </div>
+                  )}
+                  <div
+                    onClick={header.column.getToggleSortingHandler()} 
+                    className={`${header.column.getCanSort() ? 'cursor-pointer' : ''} pt-1 pb-1`}
+
+                  >
+                    {flexRender(header.column.columnDef.header, header.getContext())}
+                    {{
+                      asc: ' 🔼',
+                      desc: ' 🔽',
+                    }[header.column.getIsSorted() as string] ?? null}
+                  </div>
+                  
+                </th>
+              ))}
+            </tr>
+          ))}
+        </thead>
+        
+        <tbody>
+          {table.getRowModel().rows.map((row) => (
+            <tr key={row.id} className="even:bg-gray-100 odd:bg-white dark:even:bg-neutral-800 dark:odd:bg-black">
+              {row.getVisibleCells().map((cell, index) => (
+                <td key={cell.id} style={{ width: cell.column.getSize() }}   
+                className="overflow-hidden text-ellipsis whitespace-wrap p-2"
+                  >{flexRender(cell.column.columnDef.cell, cell.getContext())}</td>
+              ))}
+            </tr>
+          ))}
+        </tbody>
+      </table>
+      <div className='flex flex-row items-center justify-center gap-4 p-4'>
+        <CustomButton
+          onClick={() => table.setPageIndex(0)}
+          disabled={!table.getCanPreviousPage()}
+        >
+          First
+        </CustomButton>
+        <CustomButton 
+          onClick={() => table.previousPage()}
+          disabled={!table.getCanPreviousPage()}
+        >
+          Prev
+        </CustomButton>
+        <span>
+          Page <strong>{table.getState().pagination.pageIndex + 1}</strong> of{" "}
+          {table.getPageCount()}
+        </span>
+        <CustomButton
+          onClick={() => table.nextPage()}
+          disabled={!table.getCanNextPage()}
+        >
+          Next
+        </CustomButton>
+        <CustomButton
+          onClick={() => table.setPageIndex(table.getPageCount() - 1)}
+          disabled={!table.getCanNextPage()}
+        >
+          Last
+        </CustomButton>
+      </div>
+    </div>
+  );
+}
